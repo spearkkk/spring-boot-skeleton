78d5dfff7463024e72904d8e0d031e6affb3f337
spearkkk <spearkkp@gmail.com>
2020-11-27 17:02:36 +0900

    Initialize spring boot and add HelloController
    HelloController is for health check and test is added.
    And lombok is added too.

---
+++ src/test/groovy/com/spearkkk/skeleton/web/HelloControllerTest.groovy
@@ -0,0 +1,23 @@
+package com.spearkkk.skeleton.web
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import spock.lang.Specification
+
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest extends Specification {
+    @Autowired
+    protected MockMvc mockMvc
+
+    def "HelloController should return 'hello, dude!' for '/hello'."() {
+        when:
+        def result = mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+
+        then:
+        result.andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("hello, dude!"))
+    }
+}
